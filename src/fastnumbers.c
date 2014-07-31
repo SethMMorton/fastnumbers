f936e670e2eed77b02141306073bf73f2ed030d9
Seth M Morton <seth.m.morton@gmail.com>
2014-07-30 23:35:32 -0700

    Fixed segfault.
    A segfault occurred because the returned values were not being properly
    reference counted.  This was fixed by using the Py_BuildValue function.

--- src/fastnumbers.c
+++ src/fastnumbers.c
@@ -12,20 +12,11 @@
  * Taken from SWIG wrapper output.
  */
 #if PY_VERSION_HEX >= 0x03000000
-#define PyClass_Check(obj) PyObject_IsInstance(obj, (PyObject *)&PyType_Type)
-#define PyInt_Check(x) PyLong_Check(x)
 #define PyInt_AsLong(x) PyLong_AsLong(x)
 #define PyInt_FromLong(x) PyLong_FromLong(x)
-#define PyInt_FromSize_t(x) PyLong_FromSize_t(x)
-#define PyString_Check(name) PyBytes_Check(name)
 #define PyString_FromString(x) PyUnicode_FromString(x)
-#define PyString_Format(fmt, args)  PyUnicode_Format(fmt, args)
 #define PyString_AsString(str) PyBytes_AsString(str)
-#define PyString_Size(str) PyBytes_Size(str)    
-#define PyString_InternFromString(key) PyUnicode_InternFromString(key)
-#define Py_TPFLAGS_HAVE_CLASS Py_TPFLAGS_BASETYPE
 #define PyString_AS_STRING(x) PyUnicode_AS_STRING(x)
-#define _PyLong_FromSsize_t(x) PyLong_FromSsize_t(x)
 #define PyFloat_FromString_2_3(x) PyFloat_FromString(x)
 #else
 /* The PyFloat_FromString API changed between Python2 and Python3. */
@@ -61,15 +52,15 @@ const char asfloat_docstring[] =
 static PyObject *
 fastnumbers_asfloat(PyObject *self, PyObject *args)
 {
-    PyObject *input, *result;
+    PyObject *input = NULL, *result = NULL;
     /* Read the function argument. */
     if (!PyArg_ParseTuple(args, "O", &input)) return NULL;
     /* Attempt the conversion to a float. */
     result = PyFloat_FromString_2_3(input);
     /* If unsuccessful, clear error stack and return input as-is */
-    if (result == NULL) { PyErr_Clear(); return input; }
+    if (result == NULL) { PyErr_Clear(); return Py_BuildValue("O", input); }
     /* Otherwise, return the float object */
-    return result;
+    return Py_BuildValue("O", result);
 }
 
 
