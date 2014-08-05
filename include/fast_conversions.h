e99df9aad48ab4731a88402b379d4b3b0365aa8e
Seth M Morton <seth.m.morton@gmail.com>
2014-08-04 19:02:47 -0700

    Fixed bool definitions to not conflict with gcc.

--- include/fast_conversions.h
+++ include/fast_conversions.h
@@ -4,15 +4,39 @@
 #ifdef __cplusplus
 extern "C" {
 #else
+
 /* If using library in C, use stdbool.h if available */
 #if __STDC_VERSION__ >= 199901L
 #include <stdbool.h>
 #else
-typedef int bool;
-const bool false = 0;
-const bool true  = 1;
+
+#ifndef __GNUC__
+/* _Bool builtin type is included in GCC */
+/* ISO C Standard: 5.2.5 An object declared as 
+type _Bool is large enough to store 
+the values 0 and 1. */
+/* We choose 8 bit to match C++ */
+/* It must also promote to integer */
+typedef int8_t _Bool;
+#endif /* __GNUC__ */
+
+/* ISO C Standard: 7.16 Boolean type */
+#ifndef bool
+#define bool _Bool
 #endif
+#ifndef true
+#define true 1
 #endif
+#ifndef false
+#define false 0
+#endif
+#ifndef __bool_true_false_are_defined
+#define __bool_true_false_are_defined 1
+#endif
+
+#endif /* __STDC_VERSION__ >= 199901L */
+
+#endif /* __cplusplus */
 
 /* The actual declarations */
 long fast_atoi(char *c, bool *error);
