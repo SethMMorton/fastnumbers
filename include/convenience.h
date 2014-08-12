47deac723138963fcaa0fc5450f7b9269422e8f6
Seth M Morton <seth.m.morton@gmail.com>
2014-08-11 20:26:17 -0700

    Added missing Py_TOLOWER from Python 2.6.

--- include/convenience.h
+++ include/convenience.h
@@ -12,6 +12,13 @@ extern "C" {
  * MACROS *
  **********/
 
+/* Convert character to lower.  Only needed for Python 2.6. */
+#if PY_MAJOR_VERSION == 2 || PY_MINOR_VERSION == 6
+#define Py_CHARMASK(c) ((unsigned char)((c) & 0xff))
+extern const unsigned char _Py_ctype_tolower[256];
+#define Py_TOLOWER(c) (_Py_ctype_tolower[Py_CHARMASK(c)])
+#endif
+
 /* Check if a number is an int */
 #if PY_MAJOR_VERSION >= 3
 #define PYINT_CHECK(x) PyLong_Check(x)
