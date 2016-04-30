cf7c7c82ace0f0df1b996676cad8799e4ea90932
Seth M Morton <seth.m.morton@gmail.com>
2016-04-30 16:50:03 -0700

    Fixed linking error on windows with Python 2.6.
    The workaround implementation or Py_ToLower created a linking error.
    It was reimplemented with tolower from ctypes.h.

--- src/parsing.c
+++ src/parsing.c
@@ -2,9 +2,8 @@
 
 /* Macros to convert character to lower.  Only needed for Python 2.6. */
 #if PY_MAJOR_VERSION == 2 && PY_MINOR_VERSION == 6
-#define Py_CHARMASK(c) ((unsigned char)((c) & 0xff))
-extern const unsigned char _Py_ctype_tolower[256];
-#define Py_TOLOWER(c) (_Py_ctype_tolower[Py_CHARMASK(c)])
+#include <ctype.h>
+#define Py_TOLOWER(c) tolower(c)
 #endif
 
 bool
@@ -18,7 +17,6 @@ case_insensitive_match(const char *s, const char *t)
 }
 
 #if PY_MAJOR_VERSION == 2 && PY_MINOR_VERSION == 6
-#undef Py_CHARMASK
 #undef Py_TOLOWER
 #endif
 
