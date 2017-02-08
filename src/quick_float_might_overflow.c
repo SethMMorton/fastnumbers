ad2fbe3ecdeefdf1067f6bc571aa4aaf94e399ea
Seth M Morton <seth.m.morton@gmail.com>
2017-02-07 22:02:07 -0800

    Yet another float size reduction.

--- src/quick_float_might_overflow.c
+++ src/quick_float_might_overflow.c
@@ -29,12 +29,12 @@ float_might_overflow(const char *str, const char *end)
         register unsigned len2 = 0;
         (void) consume_sign(str);
         len2 = (unsigned) (end - str);
-        /* Negative exponential can handle up to 98 (37 on MSVC). */
+        /* Negative exponential can handle up to 98 (22 on MSVC). */
         if (negative)
             exp_ok = (len2 > 0 && len2 < 2) ||
 #ifdef _MSC_VER
-                     (len2 == 2 && (*str <= '2' ||
-                                        (*str == '3' && *(str + 1) <= '7')
+                     (len2 == 2 && (*str <= '1' ||
+                                        (*str == '2' && *(str + 1) <= '2')
 #else
                      (len2 == 2 && (*str <= '8' ||
                                         (*str == '9' && *(str + 1) <= '8')
@@ -47,8 +47,8 @@ float_might_overflow(const char *str, const char *end)
     }
 
 #ifdef _MSC_VER
-    /* To be safe, we only allow up to four less than max double length. */
-    return len >= DBL_DIG - 4 || !exp_ok;
+    /* To be safe, we only allow up to five less than max double length. */
+    return len >= DBL_DIG - 5 || !exp_ok;
 #else
     /* To be safe, we only allow up to three less than max double length. */
     return len >= DBL_DIG - 3 || !exp_ok;
