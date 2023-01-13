ddcd409de8bec574ef2feaaac007e8980a734ff3
Seth Morton <seth.m.morton@gmail.com>
2023-01-12 21:49:22 -0800

    Enable running gdb as part of testing
    This is only enabled in tox, and will run pytest through gdb if gdb is
    availabel, otherwise just runs pytest directly.
    
    The benefit is that if a segfault happens, we can easily spot the reason
    why.

---
+++ dev/test-runner.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+"""
+This script will run tests using gdb as a means to catch segfaults.
+
+If gdb is not installed, it just runs the tests.
+"""
+
+import os
+import sys
+
+my_python = sys.argv[1]
+other_args = sys.argv[2:]
+try:
+    # Attempt to run pytest with debugger
+    os.execlp(
+        "gdb",
+        "gdb",
+        "-ex",
+        "run",
+        "-ex",
+        "bt",
+        "-ex",
+        "quit",
+        "--args",
+        my_python,
+        "-m",
+        "pytest",
+        "--doctest-glob=README.rst",
+        *other_args,
+    )
+except OSError:
+    # No debugger installed, just run pytest directly
+    os.execl(
+        my_python, my_python, "-m", "pytest", "--doctest-glob=README.rst", *other_args
+    )
